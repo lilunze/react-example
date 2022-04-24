926e782c66c371b9f28adf9ac59a159461db9585
lilunze <3737@winnermedical.com>
2022-04-24 17:30:15 +0800

    login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,11 +3,45 @@
  * @LastEditors: lilunze
  * @Desc: 登陆界面
  */
+import { Input, Button } from "@nutui/nutui-react";
+import { useState } from "react";
+import style from "./index.module.css";
 
 export default function Login() {
-    return (
-        <div>
-            login
-        </div>
-    );
+  const [user, UpdateUser] = useState("");
+  const [password, UpdatePassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  return (
+    <div>
+      <div>
+        <img
+          className={style.logo_img}
+          src={require("../../assets/contact/w-logo.png")}
+          alt=""
+        />
+      </div>
+      <div>
+        <Input label="用户名" defaultValue={user} requireShow />
+        <Input
+          label="密码"
+          defaultValue={password}
+          requireShow
+          type="password"
+        />
+        <Button
+          loading={loading}
+          type="success"
+        //   onClick={() => {
+        //     setTimeout(() => {
+        //       setLoading(false);
+        //     }, 1500),
+        //       setLoading(!loading);
+        //   }}
+        //   style={{ margin: 8 }}
+        >
+          Click me!
+        </Button>
+      </div>
+    </div>
+  );
 }
